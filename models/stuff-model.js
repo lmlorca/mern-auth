35ad91f54001f8cb99aaba0965fb0311e2d01848
lmlorca <luismlorca@gmail.com>
2019-06-15 23:06:33 -0400

    Cleaned stuff.

--- models/stuff-model.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose')
-
-const stuffSchema = Schema({
-  name: { type: String, required: true },
-  price: Number
-})
-
-const Stuff = model('Stuff', stuffSchema)
-module.exports = Stuff
--- server.js
+++ server.js
@@ -26,23 +26,4 @@ app.use(
 
 /***/
 
-app.use('/stuffs/', require('./routes/stuff-route'))
-
-const EmailsList = require('./models/emails-model')
-app.post('/seed-emails/', (req, res, next) => {
-  let emails = new EmailsList({
-    emails: [
-      'luismlorca@gmail.com',
-      'llorca@truenorthcorporation.com',
-      'llorcatnc@outlook.com'
-    ]
-  })
-  emails.save(null, (err, emails) => {
-    if (err) next(err)
-    else res.json(emails)
-  })
-})
-
-/***/
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
